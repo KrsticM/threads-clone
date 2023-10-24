937600ee6c8ebe356147c27203c88a6338584eb3
Milos Krstic <milos.krstic@symphony.is>
2023-10-24 14:56:40 +0200

    Initial commit

---
+++ threads-clone/App/threads_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  threads_cloneApp.swift
+//  threads-clone
+//
+//  Created by SYM0271 on 3.10.23..
+//
+
+import SwiftUI
+
+@main
+struct threads_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
